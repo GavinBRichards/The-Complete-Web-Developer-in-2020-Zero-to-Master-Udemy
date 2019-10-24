1b9a8d91a1f0f47c44270630b57e245140232fe6
Gavin Richards <hillarygavin@gmail.com>
2019-10-24 16:12:54 -0600

    Ex1-3

---
+++ JavaScript/Ex3.js
@@ -0,0 +1,13 @@
+// Make a Calculator! using prompt(), and variables, make a program that does the following:
+// 1. Prompts the user for first number.
+// 2. Stores that first number
+// 3. Prompts the user for the second number.
+// 4. stores that number and responds with the SUM by using an alert.
+
+//
+//
+//
+
+var firstNumber = prompt("What is the first number?");
+var secondNumber = prompt("What is the second number?");
+alert(Number(firstNumber) + Number(secondNumber));
