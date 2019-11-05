96c47a6a1d1bb1da602dbd6e38e622bcf3fbaea3
Gavin Richards <hillarygavin@gmail.com>
2019-11-05 08:08:46 -0700

    facebook login loop

--- JavaScript/FaceBook/script.js
+++ JavaScript/FaceBook/script.js
@@ -2,6 +2,14 @@ var database = [
   {
     username: "gavin",
     password: "supersecret"
+  },
+  {
+    username: "sally",
+    password: "123"
+  },
+  {
+    username: "ingrid",
+    password: "777"
   }
 ];
 
@@ -13,18 +21,34 @@ var newsFeed = [
   {
     username: "Sally",
     timeline: "Javascript is soooo cool!"
+  },
+  {
+    username: "Mitch",
+    timeline: "Javascript is okay!"
   }
 ];
 
-var userNamePrompt = prompt("What's your username?");
-var passwordPrompt = prompt("What's your password?");
+function isUserValid(username, password) {
+  for (var i = 0; i < database.length; i++) {
+    if (
+      database[i].username === username &&
+      database[i].password === password
+    ) {
+      return true;
+    }
+  }
+  return false;
+}
 
 function signIn(username, password) {
-  if (username === database[0].username && password === database[0].password) {
+  if (isUserValid(username, password)) {
     console.log(newsFeed);
   } else {
     alert("Sorry, wrong username and password!");
   }
 }
+
+var userNamePrompt = prompt("What's your username?");
+var passwordPrompt = prompt("What's your password?");
 
 signIn(userNamePrompt, passwordPrompt);
