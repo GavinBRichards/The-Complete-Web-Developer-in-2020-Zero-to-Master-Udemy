6d27241425172dff83d72a51f7c1e90b72c3b61f
Gavin Richards <hillarygavin@gmail.com>
2019-11-05 10:04:03 -0700

    Refactoring and Caching JS

--- DOM/script.js
+++ DOM/script.js
@@ -1,15 +1,33 @@
-// var button = document.getElementsByTagName("button")[0];
-
-// button.addEventListener("click", function() {
-//   console.log("CLICK!!");
-// });
-
+// Caching
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
-button.addEventListener("click", function() {
+// Do not Repeat Yourself D.R.Y
+// Refactor
+function inputLength() {
+  return input.value.length;
+}
+
+function createListElement() {
   var li = document.createElement("li");
   li.appendChild(document.createTextNode(input.value));
   ul.appendChild(li);
-});
+  input.value = "";
+}
+
+function addListAfterClick() {
+  if (inputLength() > 0) {
+    createListElement();
+  }
+}
+
+function addListAfterKeyPress(event) {
+  if (inputLength() > 0 && event.keyCode === 13) {
+    createListElement();
+  }
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeyPress);
